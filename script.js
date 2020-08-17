b1bff88a0652a3ac913ba26f9849a5d21d12f06c
saif420 <saifmahmud42069@gmail.com>
2020-08-17 22:21:27 +0600

    work finished

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ button.addEventListener("click", function(){
     fetch('https://api.lyrics.ovh/suggest/'+search)
     .then(response => response.json())
     .then(data => {
-        titleNameGet("title-0", "album-0", "lyric-0", 0);
-        titleNameGet("title-1", "album-1", "lyric-1", 1);
-        titleNameGet("title-2", "album-2", "lyric-2", 2);
-        titleNameGet("title-3", "album-3", "lyric-3", 3);
-        titleNameGet("title-4", "album-4", "lyric-4", 4);
-        titleNameGet("title-5", "album-5", "lyric-5", 5);
-        titleNameGet("title-6", "album-6", "lyric-6", 6);
-        titleNameGet("title-7", "album-7", "lyric-7", 7);
-        titleNameGet("title-8", "album-8", "lyric-8", 8);
-        titleNameGet("title-9", "album-9", "lyric-9", 9);
+        titleNameGet("title_0", "album_0", "lyric_0", 0);
+        titleNameGet("title_1", "album_1", "lyric_1", 1);
+        titleNameGet("title_2", "album_2", "lyric_2", 2);
+        titleNameGet("title_3", "album_3", "lyric_3", 3);
+        titleNameGet("title_4", "album_4", "lyric_4", 4);
+        titleNameGet("title_5", "album_5", "lyric_5", 5);
+        titleNameGet("title_6", "album_6", "lyric_6", 6);
+        titleNameGet("title_7", "album_7", "lyric_7", 7);
+        titleNameGet("title_8", "album_8", "lyric_8", 8);
+        titleNameGet("title_9", "album_9", "lyric_9", 9);
 
-        function titleNameGet(title-no, album-no, lyric-no, No){
-            document.getElementById(title-no).innerText = data.data[No].title + ", " + data.data[No].album.title;
-            document.getElementById(album-no).innerText = data.data[No].artist.name;
-            document.getElementById(lyric-no).addEventListener("click", function(){
+        function titleNameGet(title_no, album_no, lyric_no, No){
+            document.getElementById(title_no).innerText = data.data[No].title + ", " + data.data[No].album.title;
+            document.getElementById(album_no).innerText = data.data[No].artist.name;
+            document.getElementById(lyric_no).addEventListener("click", function(){
                 document.getElementById("song-tittle").innerText = data.data[No].title;
                 fetch("https://api.lyrics.ovh/v1/"+data.data[No].artist.name+"/"+data.data[No].title)
                 .then(res => res.json())
                 .then(data => {
                     console.log(data)
-                    document.getElementById("lyricsText").innerText = data.lyrics;
+                    document.getElementById("lyrics-text").innerText = data.lyrics;
                 })
             })
         }
